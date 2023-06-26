6b1ec58ec00c6022f203d87ddbed103f63a3279e
Fkadmuz <flaviohenriquedias10@gmail.com>
2023-06-25 21:53:23 -0300

    alterações

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,3 +1,42 @@
+// contagemRegressiva(50);
+
+/* 
+ * Formulário envio de dados para cálculo da média 
+ */
+const formulario1 = document.getElementById('formulario-01');
+
+if(formulario1)
+    formulario1.addEventListener('submit', function( evento ){
+
+        evento.preventDefault();
+        evento.stopPropagation();
+
+        if( this.getAttribute('class').match(/erro/) ) {
+            return false;
+        }
+
+        let dados = new FormData(this);
+
+        let notas = [];
+
+        for(let key of dados.keys()) {
+
+            let numero = dados.get(key).match(/\d*/) ? Number(dados.get(key)) : 0; // é um número
+
+            if(!isNaN(numero)) {
+                notas.push(numero);
+            }
+
+        }
+
+        console.log(notas);
+
+        texto = aprovacao(notas)
+
+        document.getElementById('resultado').innerHTML = texto;
+
+    });
+
 function validaCampo(elemento){
 
     elemento.addEventListener('focusout', function(event) {
